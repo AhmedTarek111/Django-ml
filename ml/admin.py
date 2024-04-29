f9b7740ac81ef1fbb4c279a9537bcf94a85cd478
AhmedTarek111 <ahmedtarekalsaudi@gmail.com>
2024-04-29 03:40:50 +0300

    integrate machine learning model with django

---
+++ ml/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Iris
+
+admin.site.register(Iris)
---
+++ ml/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+import pandas as pd
+import pickle
+from .models import Iris
+from .forms import IrisForm
+
+
+def iris_prediction(request):
+    if request.method == "POST":
+        form = IrisForm(request.POST)
+        if form.is_valid():    
+            form.save(commit=False)
+            sepal_length=form.cleaned_data['sepal_length']
+            sepal_width=form.cleaned_data['sepal_width']
+            petal_length=form.cleaned_data['petal_length']
+            petal_width=form.cleaned_data['sepal_length']
+            model= pd.read_pickle("model.pickle")
+            prediction = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+            result = prediction[0]  
+            form.classifcation = result  
+            form.save()
+            return render(request ,'iris.html' , {'form':form,'result':result})
+            
+        
+    else:
+        form = IrisForm()
+    return render(request ,'iris.html' , {'form':form})
+
+
